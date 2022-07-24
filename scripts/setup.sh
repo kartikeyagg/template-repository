7e875b6d051c02821ab76764c51a8f66293bd6c4
hituzi no sippo <dev@hituzi-no-sippo.me>
2022-07-24 09:55:57 +0900

    build(setup): add setup shell

---
+++ scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+function info() {
+	printf "\n%s\n" "${1}"
+}
+
+function setup() {
+	info "Setup Start"
+
+	info "Setup Complete"
+}
+
+setup
