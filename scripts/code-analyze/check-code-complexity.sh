52d8483a992946b24f8adf01765f46b582cb0621
hituzi no sippo <dev@hituzi-no-sippo.me>
2022-12-08 17:03:12 +0900

    test(code-complexity): add limit of lines of code, CCN and etc.
    Limit
    
    - lines of code without comments and blank line (`nloc`): 40
    - lines of code (`length`): 80
    - Cyclomatic Complexity Number (`CCN`): 10
    - parameter count (`parameter_count`): 5
    - token count (`token_count`): 100
    - nest (`max_nested_structures`): 3
    
    The limit is one function unit.
    
    References
    
    - https://github.com/terryyin/lizard

---
+++ scripts/code-analyze/check-code-complexity.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+lizard \
+	--Threshold nloc=40 \
+	--Threshold length=80 \
+	--Threshold cyclomatic_complexity=10 \
+	--Threshold parameter_count=5 \
+	--Threshold token_count=100 \
+	-ENS --Threshold max_nested_structures=3 \
+	"$@"
